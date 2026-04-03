06e3e5888774dbff5618d84e525424640988f5a7
miaar <miaark952@gmail.com>
2026-04-03 13:19:49 -0400

    Colored markers

--- app/src/main/java/edu/nd/pmcburne/hello/MainActivity.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/MainActivity.kt
@@ -1,19 +1,15 @@
 package edu.nd.pmcburne.hello
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -29,18 +25,13 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.room.Room
@@ -52,7 +43,6 @@ import com.google.maps.android.compose.MarkerState
 import com.google.android.gms.maps.model.LatLng
 import edu.nd.pmcburne.hello.ui.theme.MyApplicationTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
@@ -198,8 +188,6 @@ fun MapView(locations: List<LocationEntity>, selectedTag: String){
 
         ) {
             filtered.forEach { loc ->
-                val isSelected = selectedLocation == loc
-
                 Marker(
                     state = MarkerState(LatLng(loc.latitude, loc.longitude)),
                     title = loc.name,
@@ -215,7 +203,9 @@ fun MapView(locations: List<LocationEntity>, selectedTag: String){
                         }
                     )
                 )
+
             }
+
         }
 
         Button(
